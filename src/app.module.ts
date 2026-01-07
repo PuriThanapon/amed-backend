00a4e5d6fbb5d0ca3652cc9c82540bb94295a11c
PuriThanapon <purithanapon@gmail.com>
2026-01-07 09:58:37 +0700

    prometheus status up

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,14 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { PrometheusModule } from '@willsoto/nestjs-prometheus';
 
 @Module({
-  imports: [],
+  imports: [
+    PrometheusModule.register({
+      path: '/metrics', // กำหนด path สำหรับ metrics
+    }),
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
